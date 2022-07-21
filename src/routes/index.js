bcbeecd42c93fb1ff7c99b40d76bede7cd4474bc
Logu R <55403931+Logu-fosablanca@users.noreply.github.com>
2022-07-21 11:04:23 +0530

    added report

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,14 @@ import { Router } from "express";
 import { logEdiGuideCreation } from "../controllers/logEdiGuideCreationController.js";
 import { getLatestVersion } from "../controllers/getLatestVersionController.js";
 import { getEdiGuideLog } from "../controllers/getEdiGuideLogController.js";
+import { getReport } from "../controllers/getReportController.js";
+
 const app = Router();
 
 app.post('/ediLog/create',logEdiGuideCreation );
 app.post('/ediLog/get', getEdiGuideLog);
 app.post('/ediLog/latestFileVersion',getLatestVersion);
+app.post('/report/get',getReport);
 
 
 
